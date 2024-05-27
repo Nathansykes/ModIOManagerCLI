5bdca6cf1b33bcc4e684b2dc989e2eae38caa786
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-05-27 12:57:34 +0100

    opton

--- src/Commands/ModCommands.cs
+++ src/Commands/ModCommands.cs
@@ -7,9 +7,9 @@ public class ModCommands(ModService modService) : CommandBase
     private readonly ModService _modService = modService;
 
     [Command("mods")]
-    public async Task Mods()
+    public async Task Mods([Option]int? gameid = null)
     {
-        var mods = await _modService.GetSubscribedMods(null);
+        var mods = await _modService.GetSubscribedMods(gameid);
         foreach (var mod in mods)
         {
             Console.WriteLine(mod.Name);
